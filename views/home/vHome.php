27c71a3e542bdf5be4fca22fd015302b2fdf4793
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-06-04 14:30:37 +0700

    Add files via upload

--- views/home/vHome.php
+++ views/home/vHome.php
@@ -1,1 +1,282 @@
-a
+
+<main id="main">
+
+  <!-- ======= Portfolio Section ======= -->
+  <section id="pilihdesain" class="portfolio mt-5">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Buat Undangan Digitalmu</h2>
+        <p>Hanya satu menit</p>
+        <div class="center-button">
+          <a href="#"><button class="button-preview2">Buat Sekarang</button></a>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12 d-flex justify-content-center">
+          <ul id="portfolio-flters">
+            <li data-filter="*" class="filter-active">All</li>
+            <li data-filter=".silver-pack">Silver</li>
+            <li data-filter=".gold-pack">Gold</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row portfolio-container">
+
+        <div class="col-lg-4 col-md-6 portfolio-item silver-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a> 
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">SILVER</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item silver-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a> 
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">SILVER</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item silver-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a>  
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">SILVER</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item gold-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a>  
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">GOLD</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item gold-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a>  
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">GOLD</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item gold-pack">
+          <div class="portfolio-wrap">
+            <img src="<?php echo base_url(); ?>assets/home/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <div class="portfolio-links">
+                <a href="#"><button class="button-preview1">Preview</button></a>  
+              </div>
+            </div>
+          </div>
+          <div align="center" class="mt-2">
+            <h5>Classy Marble</h5>
+            <p style="color: #f59581;">GOLD</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Portfolio Section -->
+
+  <!-- ======= Fitur Section ======= -->
+  <section id="fitur" class="services section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Fitur Undangan</h2>
+        <p>Temukan Fitur-Fitur Menarik Yang Akan Membuat Undangan Pernikahan Anda Tampil Beda.</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-wallet2" style="color: #cfac5d;"></i></div>
+            <h4 class="title"><a href="">MURAH</a></h4>
+            <p class="description">Menggunakan undangan online akan jauh menghemat budget menikah Anda.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-clock" style="color: #cfac5d;"></i></div>
+            <h4 class="title"><a href="">HEMAT WAKTU</a></h4>
+            <p class="description">Jangan habiskan waktu produktifmu hanya untuk berkeliling menyebarkan undangan.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-wow-delay="0.1s">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-stack" style="color: #cfac5d;"></i></div>
+            <h4 class="title"><a href="">BANYAK PILIHAN DESAIN</a></h4>
+            <p class="description">Desain undangan simple, desain undangan minimalis dan masih banyak lagi.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6" data-wow-delay="0.1s">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-hand-thumbs-up" style="color:#cfac5d;"></i></div>
+            <h4 class="title"><a href="">MUDAH</a></h4>
+            <p class="description">Membuat undangan di Invitaco tidak perlu ribet dan user friendly.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-wow-delay="0.2s">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-shield-check" style="color: #cfac5d;"></i></div>
+            <h4 class="title"><a href="">AMAN</a></h4>
+            <p class="description">Undangan digital kamu dapat dikirimkan tanpa harus bertemu langsung.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6" data-wow-delay="0.2s">
+          <div class="icon-box">
+            <div class="icon"><i class="bi bi-flower1" style="color: #cfac5d;"></i></div>
+            <h4 class="title"><a href="">RAMAH LINGKUNGAN</a></h4>
+            <p class="description">Undangan Invitaco sama sekali tidak menggunakan kertas. Mari lestarikan bumi.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Fitur Section -->
+
+  <!-- ======= Testimoni Section ======= -->
+  <section class="spacer">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Apa kata mereka?</h2>
+        <p>Cari tahu apa kata mereka yang telah membuat undangan digital di Invitaku.</p>
+      </div>
+
+      <div class="testimonial-section">
+        <div class="testi-user-img">
+          <div class="swiper-container gallery-thumbs">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <img class="u3" src="https://md-aqil.github.io/images/2091127763_1_1_1.jpg" alt="">
+              </div>
+              <div class="swiper-slide">
+                <img class="u1" src="https://md-aqil.github.io/images/beautiful-beauty-face-2657838.jpg" alt="">
+              </div>
+              <div class="swiper-slide">
+                <img class="u2" src="https://md-aqil.github.io/images/attractive-beautiful-beauty-1986684.jpg" alt="">
+              </div>
+
+              <div class="swiper-slide">
+                <img class="u4" src="https://md-aqil.github.io/images/beautiful-beauty-face-2657838.jpg" alt="">
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="user-saying">
+          <div class="swiper-container testimonial">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper ">
+              <!-- Slides -->
+              <div class="swiper-slide">
+                <div class="quote">
+                  <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+                  <p>
+                    “This is best and biggest unified platform
+                    for instant online admission. We can easily
+                    take admission for any course in any institute..“
+                  </p>
+                  <div class="name">-Ramkishor Verma-</div>
+                  <div class="designation">University Student</div>
+
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="quote">
+                  <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+
+                  <p>
+                    “This is best and biggest unified platform
+                    for instant online admission. We can easily
+                    take admission for any course in any institute..“
+                  </p>
+                  <div class="name">-Ramkishor Verma-</div>
+                  <div class="designation">University Student</div>
+
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="quote">
+                  <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+
+                  <p>
+                    “This is best and biggest unified platform
+                    for instant online admission. We can easily
+                    take admission for any course in any institute..“
+                  </p>
+                  <div class="name">-Ramkishor Verma-</div>
+                  <div class="designation">University Student</div>
+
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="quote">
+                  <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+
+                  <p>
+                    “This is best and biggest unified platform
+                    for instant online admission. We can easily
+                    take admission for any course in any institute..“
+                  </p>
+                  <div class="name">-Ramkishor Verma-</div>
+                  <div class="designation">University Student</div>
+
+                </div>
+              </div>
+
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination swiper-pagination-white"></div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Testimoni Section -->
---
+++ views/home/vPricing.php
@@ -0,0 +1,87 @@
+<!-- ======= Fitur Paket Harga ======= -->
+<section id="fitur" class="services mt-5">
+  <div class="container">
+
+    <div class="section-title">
+      <h2>Paket Undangan</h2>
+      <p>Kami Menawarkan Paket Harga Undangan Online Yang Bisa Anda Pilih Sesuai Dengan Kebutuhan Anda.</p>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-4 mb-5 mb-lg-0">
+       <div class="bg-white p-5 rounded-lg shadow">
+         <h1 class="h6 text-uppercase font-weight-bold mb-4">BRONZE</h1>
+         <h2 class="h1 font-weight-bold"><s>Rp300<span class="text-small font-weight-normal ml-2">.000</span></s>&nbsp;Rp240<span class="text-small font-weight-normal ml-2">.000</span></h2>
+         <div class="custom-separator my-4 mx-auto bg-primary"></div>
+         <ul class="list-unstyled my-5 text-small text-center font-weight-normal">
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Unlimited</strong> Jumlah Undangan</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>1 Acara</strong></li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Peta Lokasi</strong></li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>2</strong> Foto Slider</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>4</strong> Foto Galeri</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>4</strong> Pilihan Desain</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+          <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+        </ul>
+        <div class="center-button d-grid gap-2 mt-4 mb-4">
+          <a href="#"><button class="button-preview1">Pilih Paket</button></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 mb-5 mb-lg-0">
+     <div class="bg-white p-5 rounded-lg shadow">
+      <h1 class="h6 text-uppercase font-weight-bold mb-4">SILVER</h1>
+      <h2 class="h1 font-weight-bold"><s>Rp400<span class="text-small font-weight-normal ml-2">.000</span></s>&nbsp;Rp320<span class="text-small font-weight-normal ml-2">.000</span></h2>
+      <div class="custom-separator my-4 mx-auto bg-primary"></div>
+      <ul class="list-unstyled my-5 text-small text-center font-weight-normal">
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Unlimited</strong> Jumlah Undangan</li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>2 Acara</strong></li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Peta Lokasi</strong></li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>4</strong> Foto Slider</li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>4</strong> Foto Galeri</li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>6</strong> Pilihan Desain</li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Video</strong></li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Countdown</strong></li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Buku Tamu</strong></li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+        <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> -</li>
+      </ul>
+      <div class="center-button d-grid gap-2 mt-4 mb-4">
+        <a href="#"><button class="button-preview1">Pilih Paket</button></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 mb-5 mb-lg-0">
+   <div class="bg-white p-5 rounded-lg shadow">
+     <h1 class="h6 text-uppercase font-weight-bold mb-4">GOLD</h1>
+     <h2 class="h1 font-weight-bold"><s>Rp500<span class="text-small font-weight-normal ml-2">.000</span></s>&nbsp;Rp400<span class="text-small font-weight-normal ml-2">.000</span></h2>
+     <div class="custom-separator my-4 mx-auto bg-primary"></div>
+     <ul class="list-unstyled my-5 text-small text-center font-weight-normal">
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Unlimited</strong> Jumlah Undangan</li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>2 Acara</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Peta Lokasi</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>6</strong> Foto Slider</li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>16</strong> Foto Galeri</li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>8</strong> Pilihan Desain</li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Video</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Countdown</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Buku Tamu</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>Nama Tamu Undangan</strong></li>
+      <li class="mb-3"> <i class="fa fa-check mr-2 text-primary"></i> <strong>"Amplop" Online</strong></li>
+    </ul> 
+    <div class="center-button d-grid gap-2 mt-4 mb-4">
+      <a href="#"><button class="button-preview1">Pilih Paket</button></a>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</div>
+</section><!-- End Paket Harga Section -->
